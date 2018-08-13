471eecdb80877ea9089a8bf8a30716bf6beb0d1d
Andras Findt <anthrax.f@gmail.com>
2018-08-13 21:51:25 +0200

    test for structure check Link

--- default/role.base.js
+++ default/role.base.js
@@ -22,7 +22,7 @@ const base = {
     depositResourceAtTarget: function (creep) {
         let target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
             filter: (structure) => {
-                let length = Game.creeps.length;
+                let length = _.sum(Game.creeps, (creep) => creep.memory.role === role);
                 let isLink = structure.structureType === STRUCTURE_LINK && length > 4;
                 console.log(length, isLink);
                 return (structure.structureType === STRUCTURE_SPAWN ||
