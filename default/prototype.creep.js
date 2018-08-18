326f9b89f485a12d40f65a653ad52e97e1def43d
Andras Findt <anthrax.f@gmail.com>
2018-08-18 20:01:13 +0200

    for some reason didn't put action on harvester. :/

--- default/prototype.creep.js
+++ default/prototype.creep.js
@@ -3,7 +3,6 @@ const roles = require('setup.population');
 module.exports = function () {
     Creep.prototype.performAction = function () {
         let action = roles[this.memory.role].action;
-        console.log(action);
         if (action !== undefined) {
             action.performAction(this);
         }
