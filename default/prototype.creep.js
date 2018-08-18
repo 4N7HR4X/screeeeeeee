e43c324128ae5b37abcb20754928da5aec9faa12
Andras Findt <anthrax.f@gmail.com>
2018-08-18 19:59:49 +0200

    creep prototype

--- default/prototype.creep.js
+++ default/prototype.creep.js
@@ -3,7 +3,8 @@ const roles = require('setup.population');
 module.exports = function () {
     Creep.prototype.performAction = function () {
         let action = roles[this.memory.role].action;
-        if (action !== null) {
+        console.log(action);
+        if (action !== undefined) {
             action.performAction(this);
         }
     };
